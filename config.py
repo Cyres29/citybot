0da92ecce586a8c37579e293c5aff0c494a4f9e0
Cyres29 <151050053+Cyres29@users.noreply.github.com>
2024-09-18 11:23:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -91,21 +91,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/95df19c4d60d67d6faab6.jpg"
+    "START_IMG_URL", "https://envs.sh/qQp.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/321decfc24b08c837626c.jpg"
+    "PING_IMG_URL", "https://envs.sh/qQp.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/321decfc24b08c837626c.jpg"
+PLAYLIST_IMG_URL = "https://envs.sh/qQT.jpg"
+STATS_IMG_URL = "https://envs.sh/qQT.jpg"
+TELEGRAM_AUDIO_URL = "https://envs.sh/qQp.jpg"
+TELEGRAM_VIDEO_URL = "https://envs.sh/qQT.jpg"
+STREAM_IMG_URL = "hhttps://envs.sh/qQp.jpg"
+SOUNCLOUD_IMG_URL = "https://envs.sh/qQT.jpg"
+YOUTUBE_IMG_URL = "https://envs.sh/qQp.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://envs.sh/qQT.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://envs.sh/qQp.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://envs.sh/qQp.jpg"
 
 
 def time_to_seconds(time):
